cbd8778e1adf765d40762a25fb625c4ae4eb73ac
Mark Patricio <mark@markpatricio.com>
2021-12-02 23:14:47 +1100

    Move circleci IAM user to iam.tf

--- iam.tf
+++ iam.tf
@@ -1,3 +1,8 @@
 data "aws_iam_user" "terraform" {
   user_name = "terraform"
 }
+
+module "circleci_iam_user" {
+  source         = "./modules/circleci_iam_user"
+  s3_bucket_name = aws_s3_bucket.web_statics.id
+}
--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
 resource "aws_s3_bucket" "web_statics" {
   bucket = "markpatricio-personal-web-statics"
 }
-
-module "circleci_iam_user" {
-  source         = "./modules/circleci_iam_user"
-  s3_bucket_name = aws_s3_bucket.web_statics.id
-}
