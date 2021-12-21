27db97e09e5aa202a720ff83b480a46271e975da
Mark Patricio <mark@markpatricio.com>
2021-12-21 21:45:42 +1100

    Create route53 record from cloudfront to markpatricio.com

---
+++ locals.tf
@@ -0,0 +1,3 @@
+locals {
+  domain_name = "markpatricio.com"
+}
--- modules/static_website/outputs.tf
+++ modules/static_website/outputs.tf
@@ -2,3 +2,13 @@ output "bucket_id" {
   description = "ID of bucket created"
   value       = aws_s3_bucket.web_statics.id
 }
+
+output "cloudfront_domain_name" {
+  description = "The domain name of the cloudfront distribution"
+  value       = aws_cloudfront_distribution.distribution.domain_name
+}
+
+output "cloudfront_hosted_zone_id" {
+  description = "The hosted zone id of the cloudfront distribution"
+  value       = aws_cloudfront_distribution.distribution.hosted_zone_id
+}
