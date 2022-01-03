b7d9c92e0960fd067d9fa0f520da027a581b702e
Mark Patricio <mark@markpatricio.com>
2022-01-03 13:49:23 +1100

    Remove cert validation in terraform - will do it in namecheap
    - Need an automatic email redirect and namecheap provides this automatically

--- modules/static_website/main.tf
+++ modules/static_website/main.tf
@@ -86,7 +86,6 @@ resource "aws_cloudfront_distribution" "distribution" {
     response_code      = 200
   }
 
-  depends_on = [aws_acm_certificate_validation.cert_validation]
 }
 
 resource "aws_acm_certificate" "static_website_acm_cert" {
@@ -95,27 +94,3 @@ resource "aws_acm_certificate" "static_website_acm_cert" {
   subject_alternative_names = ["*.${var.domain_name}"]
   validation_method         = "DNS"
 }
-
-resource "aws_route53_record" "cert_validation_record" {
-  for_each = {
-    for dvo in aws_acm_certificate.static_website_acm_cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = var.route53_zone_id
-}
-
-resource "aws_acm_certificate_validation" "cert_validation" {
-  provider                = aws.us
-  certificate_arn         = aws_acm_certificate.static_website_acm_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation_record : record.fqdn]
-}
-
